90fced11faa53a0a284d69a5996f26c23a7b911e
Pete Redmond <redmondp@gmail.com>
2016-03-24 22:01:50 +0000

    (feat) timer controls styled and hooked up to redux actions 🖌

--- app/components/TimerControls.js
+++ app/components/TimerControls.js
@@ -1,10 +1,20 @@
 import React from 'react';
+const btnClass = 'btn btn--pomodoro';
 
-const Timer = () => {
+const Timer = ({
+  start,
+  stop,
+  activeTimer,
+  restart,
+}) => {
   return (
     <div className="timer-controls__container">
-      <button>Start</button>
-      <button>Reset</button>
+      {(!activeTimer) ?
+        <button className={btnClass} onClick={start}>start</button> :
+        <button className={btnClass} onClick={stop}>stop</button>
+      }
+      <a className="timer__link" onClick={restart}>restart</a>
+      <span className="timer__hint">spacebar to {(!activeTimer) ? 'start' : 'stop'} timer</span>
     </div>
   )
 };
